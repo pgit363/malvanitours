549a923e3ed5376d2a52258de21bc66ed43c2fd6
pranav32 <pranav@indigitalit.com>
2022-01-08 21:08:23 +0530

    Malvani Tours Initiated

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Validator;
+use App\Http\Controllers\BaseController as BaseController;
+
+class AuthController extends BaseController
+{
+     /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+    public function index(Request $request)
+    {
+        if ($request->privilage == 'superadmin') {
+            $user = User::paginate(10);
+            return $this->sendResponse($user, 'User successfully registered');    
+        }
+        else{
+            return $this->sendError('Unauthorized', '', 401);
+        }
+    }
+
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request){
+    	$validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if($validator->fails()){
+            return $this->sendError($validator->errors(), '', 400);       
+        }
+      
+        if (! $token = auth()->attempt($validator->validated())) {
+            return $this->sendError('Unauthorized', '', 401);
+        }
+
+        return $this->createNewToken($token, 'Logged In...!');
+    }
+
+    /**
+     * Register a User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|between:2,100',
+            'email' => 'required|string|email|max:100|unique:users',
+            'password' => 'required|string|confirmed|min:6',
+        ]);
+
+        if($validator->fails()){
+            return $this->sendError($validator->errors(), '', 400);       
+        }
+      
+        $user = User::create(array_merge(
+                    $validator->validated(),
+                    ['password' => bcrypt($request->password)]
+                ));
+
+        return $this->sendResponse($user, 'User successfully registered');        
+    }
+
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout() {
+        auth()->logout();
+
+        return $this->sendResponse(null, 'User successfully signed out');
+
+        // return response()->json(['message' => 'User successfully signed out']);
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh() {
+        return $this->createNewToken(auth()->refresh(), 'Refreshed token...!');
+    }
+
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function userProfile() {
+        return $this->sendResponse(auth()->user(), 'User Fetched..!');
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function createNewToken($token, $message){
+        $response = [
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+            'user' => auth()->user()
+        ];
+
+        return $this->sendResponse($response, $message);
+    }
+}
---
+++ database/migrations/2021_11_06_203440_address.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Address extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('address', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('country');
+            $table->string('state');
+            $table->string('city');
+            $table->string('zip');
+            $table->string('block');
+            $table->string('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
